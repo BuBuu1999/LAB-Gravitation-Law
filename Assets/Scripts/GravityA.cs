fb939f86e29e8a3b67b28dcab9c0fde352d5d08e
BuBuu1999 <buranapohrn.jaiy@bumail.net>
2025-03-10 10:01:19 +0700

    Update GravityA Script

--- Assets/Scripts/GravityA.cs
+++ Assets/Scripts/GravityA.cs
@@ -6,15 +6,23 @@ public class GravityA : MonoBehaviour
     Rigidbody rb;
     const float G = 0.00674f;
 
-    private void awake()
+    private void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-
     void Attract(GravityA other)
     {
-        Rigidbody rbother = GetComponent<Rigidbody>(); ;
+        Rigidbody rbother = GetComponent<Rigidbody>();
+
+        Vector3 direction = rb.position - other.rb.position;
+
+        float distance = direction.magnitude;
+
+        if (distance == 0) { return; }
+
+        float forcemagnitude = G * ((rb.mass * other.rb.mass) / Mathf.Pow(distance, 2));
+        Vector3 force = forcemagnitude * direction.normalized;
     }
 
 }
