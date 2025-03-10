db57117b06d56dc809edafc0112607624d536080
BuBuu1999 <buranapohrn.jaiy@bumail.net>
2025-03-10 11:06:06 +0700

    Update GravityA.cs

--- Assets/Scripts/GravityA.cs
+++ Assets/Scripts/GravityA.cs
@@ -1,19 +1,44 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GravityA : MonoBehaviour
 {
 
     Rigidbody rb;
-    const float G = 0.00674f;
+    const float G = 0.06674f;
+
+    public static List<GravityA> gravityObjectList;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+
+        if (gravityObjectList == null)
+        {
+            gravityObjectList = new List<GravityA>();
+        }
+
+        gravityObjectList.Add(this);
     }
 
+
+    private void FixedUpdate()
+    {
+        foreach (GravityA obj in gravityObjectList)
+        {
+            if(obj != this)
+            {
+                Attract(obj);
+            }
+
+        }
+
+
+    }
     void Attract(GravityA other)
     {
-        Rigidbody rbOther = GetComponent<Rigidbody>();
+        Rigidbody rbOther = other.rb;
 
         Vector3 direction = rb.position - rbOther.position;
 
@@ -23,6 +48,11 @@ public class GravityA : MonoBehaviour
 
         float forcemagnitude = G * ((rb.mass * rbOther.mass) / Mathf.Pow(distance, 2));
         Vector3 force = forcemagnitude * direction.normalized;
+        rbOther.AddForce(force);
+        
+
     }
 
 }
+
+    
